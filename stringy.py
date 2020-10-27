16fe9f714d3c26cf31db75d23368521eb522b3f9
Alison Hawke <QuantumTea@users.noreply.github.com>
2020-10-27 10:36:38 -0500

    Update stringy.py

--- stringy.py
+++ stringy.py
@@ -9,14 +9,10 @@ def get_letter_in_string(input_string, index):
 def bottles_of_beer(number_of_bottles):
     if type(number_of_bottles) == int:
         if number_of_bottles == 0:
-            # Do the thing
             return "No bottles of beer on the wall, how sad"
         if number_of_bottles == 1:
-            # Do the thing
             return "One bottle of beer on the wall"
         if number_of_bottles >= 2:
-            # Do the other thing
             return str(number_of_bottles) + " bottles of beer on the wall"
     else:
-        # Do the default
         return "This is weird"
