717a489b2d118a2a70677714cbde16e420ad0201
Alison Hawke <QuantumTea@users.noreply.github.com>
2021-03-30 14:44:53 -0500

    Update test_stringy.py

--- tests/test_stringy.py
+++ tests/test_stringy.py
@@ -16,16 +16,19 @@ class TestStringy(unittest.TestCase):
 
     def test_song_lyrics(self):
         result = bottles_of_beer(0)
-        self.assertEqual("No bottles of beer on the wall, how sad", result)
+        self.assertEqual('No bottles of beer on the wall, how sad.', result)
 
         result = bottles_of_beer(1)
-        self.assertEqual("One bottle of beer on the wall", result)
+        self.assertEqual('one bottle of beer on the wall', result)
 
         result = bottles_of_beer(33)
-        self.assertEqual("33 bottles of beer on the wall", result)
+        self.assertEqual('33 bottles of beer on the wall', result)
 
-        result = bottles_of_beer("Not a number")
-        self.assertEqual("This is weird", result)
+        result = bottles_of_beer('Not a number')
+        self.assertEqual('This is weird', result)
 
     def test_sing_the_bottles_of_beer_song(self):
         result = bottles_song(10)
+        self.assertIn("10 bottles of beer on the wall", result)
+        self.assertIn("one bottle of beer on the wall", result)
+        self.assertIn("how sad", result)
