ce7eb106660d0744c2efbb41bba05df0a0d3af30
Alison Hawke <QuantumTea@users.noreply.github.com>
2022-05-04 14:22:09 -0500

    Update test_maths.py

--- tests/test_maths.py
+++ tests/test_maths.py
@@ -8,18 +8,27 @@ class TestMaths(unittest.TestCase):
         data = [1, 2, 3, 4, 5]
         result = my_sum(data)
         self.assertEqual(15, result)
+        other_data = [6, 7, 8, 9, 10]
+        result = my_sum(other_data)
+        self.assertEqual(40, result)
 
     def test_multiply_2_numbers(self):
         result = my_multiply(2, 4)
         self.assertEqual(8, result)
+        result = my_multiply(7, 9)
+        self.assertEqual(63, result)
 
     def test_square(self):
         result = my_square(3)
         self.assertEqual(9, result)
+        result = my_square(5)
+        self.assertEqual(25, result)
 
     def test_cube(self):
         result = my_cube(3)
         self.assertEqual(27, result)
+        result = my_cube(5)
+        self.assertEqual(125, result)
 
     def test_factorial(self):
         # Use the built-in function
